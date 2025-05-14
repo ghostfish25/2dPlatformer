5cf065ec3940bf240ee73f838206c4fa9808706f
Spencer cheng <spencer.cheng@gmail.com>
2025-05-14 19:17:41 -0400

    doublejump

--- Character/player.gd
+++ Character/player.gd
@@ -2,18 +2,27 @@ extends CharacterBody2D
 
 
 
-@export var speed : float  = 200.0
-@export var jump_velocity : float = -150
+@export var speed : float  = 200
+@export var jump_velocity : float = -200
+@export var double_jump_velocity : float = -150
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped : bool = false 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		has_double_jumped = false 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true 
+			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
