3420584f08b42a1edbe6f40f71b953e3cc291a3e
Spencer cheng <spencer.cheng@gmail.com>
2025-05-19 19:45:06 -0400

    movement animation

--- Character/player.gd
+++ Character/player.gd
@@ -6,8 +6,12 @@ extends CharacterBody2D
 @export var jump_velocity : float = -200
 @export var double_jump_velocity : float = -150
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false 
+var animatation_locked : bool = false 
+var direction : Vector2 = Vector2.ZERO 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -26,10 +30,27 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right","up","down")
+	
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
+
+func update_animation():
+	if not animatation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true 
+		
